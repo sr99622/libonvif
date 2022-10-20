2029d71c207ba85133a5b02adf404a45a5d5ef2f
Stephen Rhodes <sr99622@gmail.com>
2022-10-20 12:34:53 -0400

    Changed tool linking to shared, add license headers

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,24 @@
+REM*******************************************************************************
+REM libovnif/recipe/meta.yaml
+REM
+REM Copyright (c) 2020 Stephen Rhodes 
+REM
+REM This program is free software; you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation; either version 2 of the License, or
+REM (at your option) any later version.
+REM
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+REM
+REM You should have received a copy of the GNU General Public License along
+REM with this program; if not, write to the Free Software Foundation, Inc.,
+REM 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+REM
+REM******************************************************************************/
+
 mkdir build
 cd build
 cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release -DBUILD_GUI=ON ..

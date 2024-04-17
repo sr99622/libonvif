c3396b048192c397309d5174649cbd1c3ac88c33
Roshanlal <roshanlaladchitre103@gmail.com>
2024-04-17 11:24:14 +0530

    Update alertpanel.py

--- onvif-gui/gui/panels/alertpanel.py
+++ onvif-gui/gui/panels/alertpanel.py
@@ -1,1 +1,5 @@
-
+import os
+from PyQt6.QtWidgets import QGridLayout, QWidget, QCheckBox, \
+    QLabel, QComboBox, QVBoxLayout
+from PyQt6.QtCore import Qt
+from gui.onvif.datastructures import MediaSource

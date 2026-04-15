0451f558e0a92f3fdc4fbf17caab8c1b48124018
Stephen Rhodes <sr99622@gmail.com>
2026-04-15 13:54:04 -0400

    remove depndent libs

--- assets/scripts/compile.bat
+++ assets/scripts/compile.bat
@@ -1,7 +1,6 @@
-set FFMPEG_INSTALL_DIR=%HOMEPATH%\onvif-gui-win-libs\ffmpeg
-set SDL2_INSTALL_DIR=%HOMEPATH%\onvif-gui-win-libs\sdl
 set LIBXML2_INCLUDE_DIRS=%HOMEPATH%\onvif-gui-win-libs\libxml2\include\libxml2
 set LIBXML2_LIBRARIES=%HOMEPATH%\onvif-gui-win-libs\libxml2\lib\libxml2.lib
+
 cd libonvif
 if exist build\ (
     rmdir /s /q build
@@ -10,22 +9,7 @@ if exist libonvif.egg-info\ (
     rmdir /s /q libonvif.egg-info
 )
 pip install -v .
-cd ../libavio
-if exist build\ (
-    rmdir /s /q build
-)
-if exist avio.egg-info\ (
-    rmdir /s /q avio.egg-info
-)
-pip install -v .
-cd ../kankakee
-if exist build\ (
-    rmdir /s /q build
-)
-if exist kankakee.egg-info\ (
-    rmdir /s /q kankakee.egg-info
-)
-pip install -v .
+
 cd ../onvif-gui
 if exist build\ (
     rmdir /s /q build
